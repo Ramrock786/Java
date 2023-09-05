3cf57c1ad9cb15f778a19db08c81efbd06ae239b
Ramrock786 <ram.rock786@gmail.com>
2023-09-05 02:32:17 +0000

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+
+  public static void main(String[] args) {
+
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+    System.out.println ("Hello, World!");
+    System.out.println ("This line is from new commit");
+  }
+}
